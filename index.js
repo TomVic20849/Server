7319b28b9a0fb015b2c24721489c80e86e11f8fb
Ángel del Castillo <aofcastlesoftware@gmail.com>
2023-01-16 21:33:25 +0000

    Reorganized code

--- index.js
+++ index.js
@@ -3,27 +3,13 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-  });
-
-  app.get('/', (req, res) => {
-      const id = req.params.id;
-      client.query(`SELECT * FROM rooms`, (err, result) => {
-          if (err) {
-              res.status(500).send('Error retrieving data from database');
-          } else {
-              res.send(result.rows[0]);
-          }
-      });
-  });
-  
-  //
-
-  const { Client } = require('pg')
+});
+const { Client } = require('pg')
 
 // create a new client 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 // Connect to the database
@@ -34,12 +20,23 @@ require('dotenv').config()
 
 
 app.get('/rooms/:id', (req, res) => {
-  const id = req.params.id;
-  client.query(`SELECT password, is_app_connected FROM rooms WHERE id = ${id}`, (err, result) => {
-      if (err) {
-          res.status(500).send('Error retrieving data from database');
-      } else {
-          res.send(result.rows[0]);
-      }
-  });
+    const id = req.params.id;
+    client.query(`SELECT password, is_app_connected FROM rooms WHERE id = ${id}`, (err, result) => {
+        if (err) {
+            res.status(500).send('Error retrieving data from database');
+        } else {
+            res.send(result.rows[0]);
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    const id = req.params.id;
+    client.query(`SELECT * FROM rooms`, (err, result) => {
+        if (err) {
+            res.status(500).send('Error retrieving data from database');
+        } else {
+            res.send(result.rows[0]);
+        }
+    });
 });
