ef644f8f364485c33e440107b23df8b22dbd270c
Target <tomasvicente2305@gmail.com>
2023-01-17 23:18:30 +0000

    same 5?

--- index.js
+++ index.js
@@ -40,25 +40,31 @@ app.get('/', (req, res) => {
     res.send("Welcome to the server, you probably should not be seeing this message")
 });
 
-app.put('/rooms/1/:pfp_num', (req, res) =>{
-    const pfp_num = req.params.pfp_num;
-    res.send(pfp_num);
-    client.query(`SELECT password, app_pic_number, is_app_connected, powerup_id FROM rooms WHERE rooms_id = ${id};`, (err, res) =>{
-        if(err){
-            res.status(500).send('Error, cannot send information into the database');
-        }else{
-            client.query(`UPDATE rooms SET app_pic_number = $pfp_num WHERE rooms_id = ${id};`, (err, res) =>{
-                if (err) {
-                    console.log(err.stack);
-                } else {
-                    console.log(res.rows);
-                }
-            });
-            res.send(res.rows[0]);
+app.get('/rooms/:id/app_pic_number', (req, res) => {
+    const id = req.params.id;
+    client.query(`SELECT app_pic_number FROM rooms WHERE rooms_id = ${id};`, (err, result) => {
+        if (err) {
+            res.status(500).send('Error retrieving data from database');
+        } else {
+            res.send(result.rows[0]);
         }
     });
 });
 
+app.put('/rooms/:id/app_pic_number', (req, res) => {
+    const id = req.params.id;
+    const newAppPicNumber = req.body.app_pic_number;
+    client.query(`UPDATE rooms SET app_pic_number = ${newAppPicNumber} WHERE rooms_id = ${id};`, (err, result) => {
+        if (err) {
+            res.status(500).send('Error updating data in database');
+        } else {
+            res.send({ message: 'app_pic_number updated' });
+        }
+    });
+});
+
+
+/*
 app.get('/rooms/:id/app_pic/:id', (req, res) => {
     res.send(`UPDATE rooms SET app_pic_number = 1 WHERE rooms_id = ${id};`, (err, res) => {
         if (err) {
@@ -102,5 +108,4 @@ app.get('/rooms/:id/app_pic4', (req, res) => {
     });
     res.send(result.rows[0]);
 });
-
-
+*/
