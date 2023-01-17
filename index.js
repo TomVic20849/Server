cf8d84ec3d7a9f5f7b7e1a6d76ab82cf107574c5
Target <tomasvicente2305@gmail.com>
2023-01-17 23:22:37 +0000

    same 5?

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
