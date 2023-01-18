7d9767d25d2ff772cb175326a8342b00f327bd06
Target <tomasvicente2305@gmail.com>
2023-01-18 19:01:52 +0000

    a

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get('/createRoom', (req, res) => {
 app.get('/rooms/:id/:password', (req, res) => {
     const id = req.params.id;
     const password = req.params.password;
-    client.query(`SELECT app_pic_number, is_app_connected, powerup_id FROM rooms WHERE rooms_id = ${id} AND password = ${password};`, (err, result) => {
+    client.query(`SELECT password FROM rooms WHERE rooms_id = ${id} AND password = ${password};`, (err, result) => {
         if (err) {
             res.status(500).send('Error retrieving data from database');
         } else {
